739fbd120ded98d85d56bb37937976e2f4e645b4
George Mendoza <gsmendoza@gmail.com>
2025-12-30 15:52:52 +0800

    test: move app specs to input specs

---
+++ spec/chordsheet_songbook_converter/input_spec.rb
@@ -0,0 +1,60 @@
+require "spec_helper"
+require "chordsheet_songbook_converter/input"
+
+RSpec.describe ChordsheetSongbookConverter::Input do
+  describe "#to_song" do
+    let(:input_content) { "" }
+    let(:input) { described_class.new(input_content) }
+    let(:song) { input.to_song }
+
+    context "when the input is an empty file" do
+      let(:input_content) { "" }
+
+      it "generates an empty song" do
+        expect(song.stanzas).to be_empty
+      end
+    end
+
+    context "when the input is a file has a line that is not a stanza" do
+      let(:input_content) { "X" }
+
+      it "generates an empty song" do
+        expect(song.stanzas).to be_empty
+      end
+    end
+
+    context "when the input is a file with a single empty stanza" do
+      let(:input_content) do
+        <<~STANZA
+          &
+          [Intro]
+        STANZA
+      end
+
+      it "generates a song with that stanza" do
+        expect(song.stanzas.size).to eq(1)
+        expect(song.stanzas[0].name).to eq("[Intro]")
+        expect(song.stanzas[0].cleaned_name).to eq("Intro")
+        expect(song.stanzas[0].chord_lines).to be_empty
+      end
+    end
+
+    context "when the input is a file with a single stanza with a single chord line" do
+      let(:input_content) do
+        <<~STANZA
+          &
+          [Intro]
+          Aadd9/F#
+        STANZA
+      end
+
+      it "generates a song with that chord line" do
+        expect(song.stanzas.size).to eq(1)
+        stanza = song.stanzas[0]
+        expect(stanza.name).to eq("[Intro]")
+        expect(stanza.chord_lines.size).to eq(1)
+        expect(stanza.chord_lines[0].text).to eq("Aadd9/F#")
+      end
+    end
+  end
+end
